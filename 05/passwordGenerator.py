23a5b0ca2f3266ade16123cb1b5181d08ffc77d7
Kevely Luiz S Costa <kevelyluiz@gmail.com>
2025-03-01 10:42:03 -0300

    Day 5

---
+++ 05/passwordGenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the PyPassword Generator!")
+letters = int(input("How many letters would you like in your password?\n"))
+symbols = int(input("How many symbols would you like?\n"))
+numbers = int(input("How many numbers would you like?\n"))
+print("Here is your password: {}")
